6c06ff1733485266c104237e0996a833b7576994
Tony DiCola <tony@tonydicola.com>
2018-01-05 11:31:19 -0800

    Reorder library access defines for pep8 style.

--- adabot/circuitpython_libraries.py
+++ adabot/circuitpython_libraries.py
@@ -19,12 +19,14 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+import datetime
+import sys
+
+import requests
 
 from adabot import github_requests as github
 from adabot import travis_requests as travis
-import requests
-import sys
-import datetime
+
 
 def list_repos():
     repos = []
