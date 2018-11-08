063b509c64cda7ea87d6b24b284db929c648fe42
sommersoft <sommersoft@gmail.com>
2018-11-07 21:41:54 -0600

    added output_handler; flow edits

--- adabot/circuitpython_library_download_stats.py
+++ adabot/circuitpython_library_download_stats.py
@@ -24,6 +24,7 @@ import datetime
 import sys
 import argparse
 import traceback
+import os
 
 import requests
 
@@ -37,14 +38,19 @@ cmd_line_parser = argparse.ArgumentParser(description="Adabot utility for Circui
                                           prog="Adabot CircuitPython Libraries Utility")
 cmd_line_parser.add_argument("-o", "--output_file", help="Output log to the filename provided.",
                              metavar="<OUTPUT FILENAME>", dest="output_file")
+cmd_line_parser.add_argument("-v", "--verbose", help="Set the level of verbosity printed to the command prompt."
+                             " Zero is off; One is on (default).", type=int, default=1, dest="verbose", choices=[0,1])
 
+# Global variables
+output_filename = None
+verbosity = 1
+file_data = []
 
 def get_pypi_stats(repo):
     api_url = "https://pypistats.org/api/packages/" + repo.replace("_", "-").lower() + "/recent"
     pypi_stats = requests.get(api_url, timeout=10)
     if not pypi_stats.ok:
-        print(pypi_stats)
-        return "failure"
+        return "Failed to retrieve data ({})".format(pypi_stats.text)
 
     return pypi_stats.json()["data"]["last_week"]
 
@@ -66,7 +72,7 @@ def get_bundle_stats(bundle):
                                          int(release["tag_name"][4:6]),
                                          int(release["tag_name"][6:]))
         except:
-            print("Skipping release; tag name invalid:", release["tag_name"])
+            output_handler("Skipping release. Tag name invalid:", release["tag_name"])
             continue
         if (start_date - release_date).days > 7:
             break
@@ -82,20 +88,58 @@ def get_bundle_stats(bundle):
     return stats_dict
 
 def output_handler(message="", quiet=False):
-    """Handles message output to prompt/file for print_*() functions."""
+    """Handles message output to prompt/file for functions."""
     if output_filename is not None:
         file_data.append(message)
     if verbosity and not quiet:
         print(message)
 
-if __name__ == "__main__":
-    cmd_line_args = cmd_line_parser.parse_args()
-    print("Adafruit_CircuitPython_Bundle downloads in the past week:")
-    print(get_bundle_stats("Adafruit_CircuitPython_Bundle"))
-    print()
+def run_stat_check():
+    output_handler("Adafruit Library Download Stats")
+    output_handler("Report Date: {}".format(datetime.datetime.now().strftime("%d %B %Y, %I:%M%p")))
+    output_handler()
+    output_handler("Adafruit_CircuitPython_Bundle downloads for the past week:")
+    for stat in get_bundle_stats("Adafruit_CircuitPython_Bundle").items():
+        output_handler(" * {0}: {1}".format(stat[0], stat[1]))
+    output_handler()
+
     pypi_downloads = {}
+    output_handler("Adafruit CircuitPython Library PyPi downloads for the past week: ")
     repos = cpy_libs.list_repos()
     for repo in repos:
         if cpy_libs.repo_is_on_pypi(repo):
             pypi_downloads[repo["name"]] = get_pypi_stats(repo["name"])
-    print(pypi_downloads)
+    for stat in sorted(pypi_downloads.items()):
+        output_handler(" * {0}: {1}".format(stat[0], stat[1]))
+
+if __name__ == "__main__":
+    cmd_line_args = cmd_line_parser.parse_args()
+    verbosity = cmd_line_args.verbose
+    if cmd_line_args.output_file:
+        output_filename = cmd_line_args.output_file
+    i = 0
+    try:
+        run_stat_check()
+    except:
+        if output_filename is not None:
+            exc_type, exc_val, exc_tb = sys.exc_info()
+            output_handler("Exception Occurred!", quiet=True)
+            output_handler(("-"*60), quiet=True)
+            output_handler("Traceback (most recent call last):", quiet=True)
+            tb = traceback.format_tb(exc_tb)
+            for line in tb:
+                output_handler(line, quiet=True)
+            output_handler(exc_val, quiet=True)
+
+        raise
+
+    finally:
+        if output_filename is not None:
+            with open(output_filename, 'w') as f:
+                f.write("\n".join(file_data))
+                #for line in file_data:
+                    #f.write(str(line) + "\n")
+                f.flush()
+                os.fsync(f)
+    #print()
+    #print(file_data)
