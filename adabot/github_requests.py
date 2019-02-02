5463a03c533be9c315a2f631117ca8cce9372783
sommersoft <sommersoft@gmail.com>
2019-02-02 09:07:42 -0600

    actually apply timeout to requests in helpers (git/pypi/travis)

--- adabot/github_requests.py
+++ adabot/github_requests.py
@@ -52,12 +52,10 @@ def _fix_kwargs(kwargs):
             kwargs["params"]["access_token"] = access_token
         else:
             kwargs["params"] = {"access_token": access_token}
-    if "timeout" not in kwargs:
-        kwargs["timeout"] = 30
     return kwargs
 
 def get(url, **kwargs):
-    response = requests.get(_fix_url(url), **_fix_kwargs(kwargs))
+    response = requests.get(_fix_url(url), timeout=30, **_fix_kwargs(kwargs))
     if "X-RateLimit-Remaining" in response.headers:
         remaining = int(response.headers["X-RateLimit-Remaining"])
         if remaining % 100 == 0:
@@ -65,13 +63,13 @@ def get(url, **kwargs):
     return response
 
 def post(url, **kwargs):
-    return requests.post(_fix_url(url), **_fix_kwargs(kwargs))
+    return requests.post(_fix_url(url), timeout=30, **_fix_kwargs(kwargs))
 
 def put(url, **kwargs):
-    return requests.put(_fix_url(url), **_fix_kwargs(kwargs))
+    return requests.put(_fix_url(url), timeout=30, **_fix_kwargs(kwargs))
 
 def patch(url, **kwargs):
-    return requests.patch(_fix_url(url), **_fix_kwargs(kwargs))
+    return requests.patch(_fix_url(url), timeout=30, **_fix_kwargs(kwargs))
 
 def delete(url, **kwargs):
-    return requests.delete(_fix_url(url), **_fix_kwargs(kwargs))
+    return requests.delete(_fix_url(url), timeout=30, **_fix_kwargs(kwargs))
