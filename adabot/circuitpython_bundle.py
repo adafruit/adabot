667a1ff195e57ba589ae636cdd8e84f0ed652110
Melissa LeBlanc-Williams <melissa@adafruit.com>
2019-07-09 11:55:10 -0700

    Updated Bundle Email Text with updated instractions

--- adabot/circuitpython_bundle.py
+++ adabot/circuitpython_bundle.py
@@ -332,9 +332,9 @@ def new_release(bundle, bundle_path):
 
     release_description.append("\n--------------------------\n")
 
-    release_description.append("The libraries in each release are compiled for all recent major versions of CircuitPython. Please download the one that matches the major version of your CircuitPython. For example, if you are running 3.0.0 you should download the `3.x` bundle.\n")
+    release_description.append("The libraries in each release are compiled for all recent major versions of CircuitPython. Please download the one that matches the major version of your CircuitPython. For example, if you are running 4.0.0 you should download the `4.x` bundle.\n")
 
-    release_description.append("To install, simply download the matching zip file, unzip it, and copy the lib folder onto your CIRCUITPY drive. Non-express boards such as the [Trinket M0](https://www.adafruit.com/product/3500), [Gemma M0](https://www.adafruit.com/product/3501) and [Feather M0 Basic](https://www.adafruit.com/product/2772) will need to selectively copy files over.")
+    release_description.append("To install, simply download the matching zip file, unzip it, and selectively copy the libraries you would like to install into the lib folder on your CIRCUITPY drive. This is especially important for non-express boards such as the [Trinket M0](https://www.adafruit.com/product/3500), [Gemma M0](https://www.adafruit.com/product/3501) and [Feather M0 Basic](https://www.adafruit.com/product/2772).")
 
     release = {
         "tag_name": "{0:%Y%m%d}".format(date.today()),
