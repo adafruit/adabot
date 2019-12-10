6b04be8870d999c1c575af0ff386fa65833ef6b4
dherrada <dylan.herrada@gmail.com>
2019-12-10 17:49:25 -0500

    Fixed the script a bit more

--- adabot/travis_2_actions.py
+++ adabot/travis_2_actions.py
@@ -1,5 +1,20 @@
 import subprocess
 import os
+import pickle
+
+def storeData(F, db):
+    # initializing data to be stored in db
+
+    dbfile = open(F, 'wb')
+
+    pickle.dump(db, dbfile)
+    dbfile.close()
+
+def loadData(F):
+    # loading stored data
+    dbfile = open(F, 'rb')
+    db = pickle.load(dbfile)
+    return db
 
 bashCommand = "ls repositories/"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
@@ -7,26 +22,29 @@ output, error = process.communicate()
 
 parsed = output.decode('utf8').split('\n')
 
-didnt_do = []
-didnt_commit = []
+db = loadData('db.txt')
 for repo in parsed:
+    if repo not in db.keys():
+        print(added)
+        db[repo] = {'didnt do': 0, 'added': 0, 'processed': 0, 'pushed': 0}
+
     loc = '/home/dherrada/adafruit/travis_to_actions/repositories/{}/'.format(repo)
 
     # Tests to see if there's code in the repo
 
     if not os.path.isfile(loc+'README.rst') and not os.path.isfile(loc+'README.md'):
-        didnt_do.append(repo)
+        db[repo]['didnt do'] = 1 
         continue
 
     try:
         with open(loc+'README.rst', 'r') as F:
             if  sum(1 for line in F) < 5:
-                didnt_do.append(repo)
+                db[repo]['didnt do'] = 1
                 continue
     except FileNotFoundError:
         with open(loc+'README.md', 'r') as F:
             if sum(1 for line in F) < 5:
-                didnt_do.append(repo)
+                db[repo]['didnt do'] = 1
                 continue
 
     for root, dirs, files in os.walk(loc):
@@ -37,7 +55,7 @@ for repo in parsed:
             continue
         break
     else:
-        didnt_do.append(repo)
+        db[repo]['didnt do'] = 1
 
 
     os.system('cp -r .github/ {}'.format(loc))
@@ -58,15 +76,19 @@ for repo in parsed:
     os.system("sed -i '/.. image::\|:target:/ s:travis-ci:github: ; s:.com/adafruit/{0}*$:.com/adafruit/{0}/actions/:; s:.svg?branch=master:/workflows/Build%20CI/badge.svg:' README.rst".format(repo))
 
 
+    if input("would you like to start commiting?  ").lower() != 'y':
+        break
     os.system("git add .")
+    db[repo]['added'] = 1
     os.system("git diff --cached")
     good = input("Commit?  ")
     if good.lower() == 'y':
         os.system("git commit -m 'Switched from Travis to GitHub Actions'")
-
-    else:
-        didnt_commit.append(repo)
+        db[repo]['processed'] = 1
     # Commenting this out so I don't accidentally push everything
     # os.system("git push --set-upstream origin dherrada-patch-1")
-print(didnt_do)
-print(didnt_commit)
+    # db[repo]['pushed'] = 1
+
+
+storeData('db.txt', db)
+print(loadData('db.txt'))
