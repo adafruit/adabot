127d623b471052d2d1ec06a408d0f9e1a0a17bd4
sommersoft <sommersoft@gmail.com>
2021-06-19 14:24:56 -0500

    add tests to verify report output files

--- tests/integration/test_arduino_libraries.py
+++ tests/integration/test_arduino_libraries.py
@@ -15,3 +15,20 @@ def test_adafruit_libraries(monkeypatch):
     print(arduino_libraries.list_repos())
 
     arduino_libraries.main()
+
+def test_adafruit_libraries_output_file(monkeypatch, tmp_path, capsys):
+
+    def get_list_repos():
+        repos = []
+        repos.append(github_requests.get("/repos/adafruit/Adafruit_NeoPixel").json())
+        return repos
+
+    monkeypatch.setattr(arduino_libraries, "list_repos", get_list_repos)
+
+    tmp_output_file = tmp_path / "output_test.txt"
+
+    arduino_libraries.main(output_file=tmp_output_file)
+
+    captured = capsys.readouterr()
+
+    assert tmp_output_file.read_text() == captured.out
