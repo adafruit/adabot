fba687739e9c70f46a4cc527df58858d20f3057a
Alec Delaney <tekktik@gmail.com>
2022-08-18 20:30:37 -0400

    Use CLI option to specify allowable validators

--- tests/integration/test_circuitpython_libraries.py
+++ tests/integration/test_circuitpython_libraries.py
@@ -28,28 +28,29 @@ def test_circuitpython_libraries(monkeypatch, pytestconfig):
     # Delete specific tests that require repository secrets
     # They can't be tested via, so let's remove them and test the others
     if not pytestconfig.getoption("--use-tokens"):
-        print("FDSFSD")
-        for funcname in circuitpython_library_validators.TOKEN_FUNCTIONS:
-            monkeypatch.delattr(circuitpython_library_validators.LibraryValidator, funcname)
+        vals = [validator for validator in circuitpython_libraries.default_validators if validator not in circuitpython_library_validators.TOKEN_FUNCTIONS]
+        vals_str = ",".join(vals)
+    else:
+        vals_str = "all"
 
-    circuitpython_libraries.main(validator="all")
+    circuitpython_libraries.main(validator=vals_str)
 
 
 # pylint: disable=invalid-name
 def test_circuitpython_libraries_output_file(monkeypatch, pytestconfig, tmp_path, capsys):
     """Test main funciton of 'circuitpython_libraries.py', with writing an output file."""
 
-    monkeypatch.setattr(common_funcs, "list_repos", mock_list_repos)
-
     # Delete specific tests that require repository secrets
     # They can't be tested via, so let's remove them and test the others
     if not pytestconfig.getoption("--use-tokens"):
-        for funcname  in circuitpython_library_validators.TOKEN_FUNCTIONS:
-            monkeypatch.delattr(circuitpython_library_validators.LibraryValidator, funcname)
+        vals = [validator for validator in circuitpython_libraries.default_validators if validator not in circuitpython_library_validators.TOKEN_FUNCTIONS]
+        vals_str = ",".join(vals)
+    else:
+        vals_str = "all"
 
     tmp_output_file = tmp_path / "output_test.txt"
 
-    circuitpython_libraries.main(validator="all", output_file=tmp_output_file)
+    circuitpython_libraries.main(validator=vals_str, output_file=tmp_output_file)
 
     captured = capsys.readouterr()
 
